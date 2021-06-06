2d6d6072cf556f6f1affedba48689e113196d542
takeyu1013 <yuto1013@gmail.com>
2021-06-06 17:54:32 +0900

    Next.jsをセットアップ

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Index: NextPage = () => {
+  return (
+    <div>
+      <header>
+        <h1>takeyu1013's portfolio</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Index;
