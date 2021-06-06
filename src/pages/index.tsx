4924d90dae2ff5502b013e455809459ce5348b29
takeyu1013 <yuto1013@gmail.com>
2021-06-06 21:54:52 +0900

    ヘッダーのスタイルを変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@ import type { NextPage } from "next";
 
 const Index: NextPage = () => {
   return (
-    <div>
-      <header>
-        <h1>takeyu1013's portfolio</h1>
+    <div className="h-screen">
+      <header className="bg-gradient-to-b from-green-200 to-green-500 h-1/3 items-center flex">
+        <h1 className="mx-auto text-white text-4xl font-bold flex-shrink">
+          takeyu1013's portfolio
+        </h1>
       </header>
     </div>
   );
