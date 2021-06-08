6c4c96cc5ade1c3500c40175bc0dd401c1345261
takeyu1013 <yuto1013@gmail.com>
2021-06-08 18:58:28 +0900

    レイアウトを調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,84 +8,84 @@ const Index: NextPage = () => {
           takeyu1013's portfolio
         </h1>
       </header>
-      <main>
-        <div className="profile">
-          <h2>プロフィール</h2>
-          <table>
-            <tr>
+      <main className="flex flex-wrap">
+        <div className="flex flex-col p-4">
+          <h2 className="flex-shrink mx-auto text-xl font-bold pb-2">
+            プロフィール
+          </h2>
+          <table className="mx-auto">
+            <tr className="border">
               <td>名前</td>
               <td>たけゆ</td>
             </tr>
-            <tr>
+            <tr className="border bg-green-100">
               <td>本名</td>
               <td>Yuto Takeuchi</td>
             </tr>
-            <tr>
+            <tr className="border">
               <td>出身地</td>
               <td>兵庫県</td>
             </tr>
-            <tr>
+            <tr className="border bg-green-100">
               <td>今住んでいるところ</td>
               <td>神奈川県</td>
             </tr>
-            <tr>
+            <tr className="border">
               <td>職業</td>
               <td>セールスエンジニア</td>
             </tr>
           </table>
         </div>
-        <div className="cards">
-          <div className="hobbeis">
-            <h2>趣味</h2>
-            <ul>
-              <li>プログラミング</li>
-              <li>ゲーム（FPS、アクション）</li>
-              <li>アニメ</li>
-              <li>ガジェット（自作PC等）</li>
-              <li>筋トレ</li>
-              <li>キャンプ</li>
-              <li>温泉</li>
-              <li>読書</li>
-            </ul>
-          </div>
-          <div className="skills">
-            <h2>スキル</h2>
-            <ul>
-              <li>C/C++（3年）</li>
-              <li>Java（3年）</li>
-              <li>Python（2年）</li>
-              <li>JavaScript（2年）</li>
-              <li>Linux(Ubuntu, CentOS)</li>
-              <li>PostgreSQL</li>
-              <li>Spring Boot</li>
-            </ul>
-          </div>
-          <div className="future">
-            <h2>興味がある技術</h2>
-            <ul>
-              <li>TypeScript</li>
-              <li>Next.js</li>
-              <li>Unity</li>
-              <li>Godot Engine</li>
-              <li>Terraform</li>
-              <li>GraphQL</li>
-              <li>Procreate</li>
-            </ul>
-          </div>
-          <div className="products">
-            <h2>つくったもの</h2>
-            <a href="http://todo-seven-beta.vercel.app/">やることリスト</a>
-          </div>
-          <div className="sns">
-            <h2>SNS</h2>
-            <ul>
-              <li>GitHub</li>
-              <li>Qiita</li>
-              <li>Facebook</li>
-              <li>Twitter</li>
-              <li>Instagrem</li>
-            </ul>
-          </div>
+        <div className="mx-auto p-4">
+          <h2 className="text-center text-xl font-bold pb-2">趣味</h2>
+          <ul className="pb-2">
+            <li>プログラミング</li>
+            <li>ゲーム（FPS、アクション）</li>
+            <li>アニメ</li>
+            <li>ガジェット（自作PC等）</li>
+            <li>筋トレ</li>
+            <li>キャンプ</li>
+            <li>温泉</li>
+            <li>読書</li>
+          </ul>
+        </div>
+        <div className="mx-auto p-4">
+          <h2 className="text-center text-xl font-bold pb-2">スキル</h2>
+          <ul className="pb-2">
+            <li>C/C++（3年）</li>
+            <li>Java（3年）</li>
+            <li>Python（2年）</li>
+            <li>JavaScript（2年）</li>
+            <li>Linux(Ubuntu, CentOS)</li>
+            <li>PostgreSQL</li>
+            <li>Spring Boot</li>
+          </ul>
+        </div>
+        <div className="mx-auto p-4">
+          <h2 className="text-center text-xl font-bold pb-2">興味がある技術</h2>
+          <ul>
+            <li>TypeScript</li>
+            <li>Next.js</li>
+            <li>Unity</li>
+            <li>Godot Engine</li>
+            <li>Terraform</li>
+            <li>GraphQL</li>
+            <li>Procreate</li>
+          </ul>
+        </div>
+        <div className="mx-auto p-4">
+          <h2 className="text-center text-xl font-bold pb-2">つくったもの</h2>
+          <a href="http://todo-seven-beta.vercel.app/">やることリスト</a>
+        </div>
+        <div className="mx-auto p-4">
+          <h2 className="text-center text-xl font-bold pb-2">SNS</h2>
+          <ul>
+            <li>GitHub</li>
+            <li>Qiita</li>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Instagrem</li>
+          </ul>
         </div>
       </main>
     </div>
