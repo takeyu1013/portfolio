cae60aeae3e7f10283daf81318fdd1650546e187
takeyu1013 <yuto1013@gmail.com>
2021-06-09 22:48:41 +0900

    レイアウトを調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,8 @@ const Index: NextPage = () => {
         </h1>
       </header>
       <main className="flex flex-wrap">
-        <div className="flex flex-col p-4">
-          <h2 className="flex-shrink mx-auto text-xl font-bold pb-2">
-            プロフィール
-          </h2>
+        <div className="p-4 mx-auto w-1/3">
+          <h2 className="text-center text-xl font-bold pb-2">プロフィール</h2>
           <table className="mx-auto">
             <tr className="border">
               <td>名前</td>
@@ -36,9 +34,9 @@ const Index: NextPage = () => {
             </tr>
           </table>
         </div>
-        <div className="mx-auto p-4">
+        <div className="mx-auto p-4 w-1/3">
           <h2 className="text-center text-xl font-bold pb-2">趣味</h2>
-          <ul className="pb-2">
+          <ul className="text-center pb-2">
             <li>プログラミング</li>
             <li>ゲーム（FPS、アクション）</li>
             <li>アニメ</li>
@@ -49,9 +47,9 @@ const Index: NextPage = () => {
             <li>読書</li>
           </ul>
         </div>
-        <div className="mx-auto p-4">
+        <div className="mx-auto p-4 w-1/3">
           <h2 className="text-center text-xl font-bold pb-2">スキル</h2>
-          <ul className="pb-2">
+          <ul className="text-center pb-2">
             <li>C/C++（3年）</li>
             <li>Java（3年）</li>
             <li>Python（2年）</li>
@@ -61,9 +59,9 @@ const Index: NextPage = () => {
             <li>Spring Boot</li>
           </ul>
         </div>
-        <div className="mx-auto p-4">
+        <div className="mx-auto p-4 w-1/3">
           <h2 className="text-center text-xl font-bold pb-2">興味がある技術</h2>
-          <ul>
+          <ul className="text-center pb-2">
             <li>TypeScript</li>
             <li>Next.js</li>
             <li>Unity</li>
@@ -73,13 +71,18 @@ const Index: NextPage = () => {
             <li>Procreate</li>
           </ul>
         </div>
-        <div className="mx-auto p-4">
+        <div className="mx-auto p-4 w-1/3">
           <h2 className="text-center text-xl font-bold pb-2">つくったもの</h2>
-          <a href="http://todo-seven-beta.vercel.app/">やることリスト</a>
+          <a
+            className="block text-center"
+            href="http://todo-seven-beta.vercel.app/"
+          >
+            やることリスト
+          </a>
         </div>
-        <div className="mx-auto p-4">
+        <div className="mx-auto p-4 w-1/3">
           <h2 className="text-center text-xl font-bold pb-2">SNS</h2>
-          <ul>
+          <ul className="text-center pb-2">
             <li>GitHub</li>
             <li>Qiita</li>
             <li>Facebook</li>
