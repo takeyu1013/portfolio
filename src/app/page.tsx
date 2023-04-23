f4366538296e6953ea8ee5e81bf3970e0775e88a
takeyu1013 <yuto1013@gmail.com>
2023-04-23 13:37:41 +0900

    💬

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
 const Home = () => {
-  return <main>Hell, word!</main>;
+  return (
+    <main>
+      <h1>Takeyu&apos;s portfolio</h1>
+      <h2>プロフィール</h2>
+      <table>
+        {[
+          ["名前", "たけゆ"],
+          ["本名", "Yuto Takeuchi"],
+          ["出身地", "兵庫県"],
+          ["今住んでいるところ", "神奈川県"],
+          ["職業", "セールスエンジニア"],
+        ].map(([key, value], index) => {
+          return (
+            <tr key={index}>
+              <td>{key}</td>
+              <td>{value}</td>
+            </tr>
+          );
+        })}
+      </table>
+      <h2>趣味</h2>
+      <p>WIP</p>
+      <h2>スキル</h2>
+      <p>WIP</p>
+      <h2>興味がある技術</h2>
+      <p>WIP</p>
+      <h2>つくったもの</h2>
+      <p>WIP</p>
+      <h2>SNS</h2>
+      <p>WIP</p>
+    </main>
+  );
 };
 
 export default Home;
